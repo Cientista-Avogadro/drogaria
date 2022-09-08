25e3fe98ffc29c1d7bfe047ed39bca4e5ea5b447
Cientista Avogadro <cientistaavogadro2019@gmail.com>
2022-09-08 16:42:03 +0100

    feat: funcionality create new category route

--- src/main/java/com/scientit/drogaria/controllers/CategoriaController.java
+++ src/main/java/com/scientit/drogaria/controllers/CategoriaController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class CategoriaController {
   @GetMapping
   public List<Categoria> getAllCategory() {
     return categoriaRepository.findAll();
+  }
+
+  @PostMapping
+  public Categoria postCategory(@RequestBody Categoria category) {
+    return categoriaRepository.save(category);
   }
 }
