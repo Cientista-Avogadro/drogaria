a62938c128e06e3772973b3f17baf6e638aca5ea
Cientista Avogadro <cientistaavogadro2019@gmail.com>
2022-09-08 16:18:14 +0100

    feat: Category controller GetAllCategory()

---
+++ src/main/java/com/scientit/drogaria/controllers/CategoriaController.java
@@ -0,0 +1,23 @@
+package com.scientit.drogaria.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.scientit.drogaria.domain.Categoria;
+import com.scientit.drogaria.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("categorias")
+public class CategoriaController {
+  @Autowired
+  private CategoriaRepository categoriaRepository;
+
+  @GetMapping
+  public List<Categoria> getAllCategory() {
+    return categoriaRepository.findAll();
+  }
+}
