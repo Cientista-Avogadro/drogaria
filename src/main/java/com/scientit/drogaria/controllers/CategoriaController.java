9a1722a8bdf4a971ea35223238b35217d5817eee
Cientista Avogadro <cientistaavogadro2019@gmail.com>
2022-09-08 16:53:50 +0100

    feat: funcionality delete category route

--- src/main/java/com/scientit/drogaria/controllers/CategoriaController.java
+++ src/main/java/com/scientit/drogaria/controllers/CategoriaController.java
@@ -1,12 +1,16 @@
 package com.scientit.drogaria.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.scientit.drogaria.domain.Categoria;
@@ -26,5 +30,12 @@ public class CategoriaController {
   @PostMapping
   public Categoria postCategory(@RequestBody Categoria category) {
     return categoriaRepository.save(category);
+  }
+
+  @DeleteMapping("/{codigo}")
+  public Optional<Categoria> deleteCategory(@PathVariable Short codigo) {
+    Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+    categoriaRepository.delete(categoria.get());
+    return categoria;
   }
 }
