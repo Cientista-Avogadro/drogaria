f02d648397f96d435c31650e33730f5b8d52a456
Cientista Avogadro <cientistaavogadro2019@gmail.com>
2022-09-08 14:48:04 +0100

    feat: add jpa anotattion in Product domain

--- src/main/java/com/scientit/drogaria/domain/Producto.java
+++ src/main/java/com/scientit/drogaria/domain/Producto.java
@@ -3,6 +3,12 @@ package com.scientit.drogaria.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +16,22 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Entity
 public class Producto {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Short codigo;
+
+  @Column(length = 50, unique = true, nullable = false)
   private String nome;
+
+  @Column(nullable = false)
   private Byte quantidade;
+
+  @Column(nullable = false, precision = 5, scale = 0)
   private BigDecimal preco;
+
+  @Column
   private LocalDate dataDeValidade;
 }
