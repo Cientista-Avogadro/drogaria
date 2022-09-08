8f70508cfcc16d613fde8d7011615703846fa1ed
Cientista Avogadro <cientistaavogadro2019@gmail.com>
2022-09-08 16:06:40 +0100

    feat: Categoria Repository

---
+++ src/main/java/com/scientit/drogaria/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.scientit.drogaria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.scientit.drogaria.domain.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Byte> {
+    
+}
